8de5faa57d05c9a28e0a21f7c23572672abe3056
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-08-10 16:18:25 -0300

    Update Funcao.java

--- MetodoSecante/src/metodosecante/Funcao.java
+++ MetodoSecante/src/metodosecante/Funcao.java
@@ -3,24 +3,32 @@ package metodosecante;
 import javax.swing.JOptionPane;
 import org.nfunk.jep.JEP;
 
-public class Funcao {
+public class Funcao 
+{
     JEP j = new JEP();
-    public Funcao(String def) {
+    
+    public Funcao(String def) 
+    {
         j.addVariable("x", 0);
         j.addStandardConstants();
         j.addStandardFunctions();
         j.setImplicitMul(true);
         j.parseExpression(def);
-        if (j.hasError()) {
+        
+        if (j.hasError()) 
+        {
            JOptionPane.showMessageDialog(null, "Erro ao converter a função!");
         }  
     }
      
-    public double eval(double x){
+    public double eval(double x)
+    {
         double r;
         j.addVariable("x", x);
         r = j.getValue();
-        if (j.hasError()) {
+        
+        if (j.hasError()) 
+        {
            JOptionPane.showMessageDialog(null, "Erro ao converter a função!");
         }
         return r;
