5f8742a3e6c5058fed3b00178dc1c06bf560e63d
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-08-10 16:13:02 -0300

    Update SecanteTeste.java

--- MetodoSecante/src/metodosecante/SecanteTeste.java
+++ MetodoSecante/src/metodosecante/SecanteTeste.java
@@ -2,8 +2,10 @@ package metodosecante;
 
 import javax.swing.JOptionPane;
 
-public class SecanteTeste {
-    public static void main(String[] args) {
+public class SecanteTeste 
+{
+    public static void main(String[] args) 
+    {
        Funcao f = new Funcao("sin(x)*x");
        Secante s = new Secante();
        JOptionPane.showMessageDialog(null, s.raiz(f, 1, 2, 100, 1e-6));
